39e112564077106384e3d85639b3bcb47cf523b0
Rildo Júnior <rildojuniorcontato@gmail.com>
2021-12-16 10:57:45 -0300

    create functions to calculate and clean code

--- scripts.js
+++ scripts.js
@@ -7,103 +7,47 @@ function close() {
   return close
 }
 
-// criando a função toFixedDown para não deixar arredondar
-Number.prototype.toFixedDown = function (digits) {
-  var re = new RegExp('(\\d+\\.\\d{' + digits + '})(\\d)'),
-    m = this.toString().match(re)
-  return m ? parseFloat(m[1]) : this.valueOf()
-}
-
-function calcConversion(event) {
-  event.preventDefault() // não deixa mandar os conteúdos do form
+function calculate(idName, percentage) {
   let oneBag = document.getElementById('oneBag').value
   let weightBag = document.getElementById('weightBag').value
 
-  // calculo total
+  let namePercentage = weightBag * percentage
+
+  let percSc = namePercentage / oneBag
+  percSc = parseInt(percSc)
+
+  let percKg = percSc * oneBag - namePercentage
+  percKg = parseInt(percKg * -1)
+
+  document.getElementById(`${idName}`).innerHTML = `${percSc}sc e ${percKg}kg`
+}
+
+function calculateTotalMaster(idName) {
+  let oneBag = document.getElementById('oneBag').value
+  let weightBag = document.getElementById('weightBag').value
+
   let totalMasterSc = weightBag / oneBag
-  totalMasterSc = totalMasterSc.toFixedDown(0)
+  totalMasterSc = parseInt(totalMasterSc)
 
   let totalMasterKg = totalMasterSc * oneBag - weightBag
   totalMasterKg = parseInt(totalMasterKg * -1)
 
   document.getElementById(
-    'totalMaster'
+    `${idName}`
   ).innerHTML = `${totalMasterSc}sc e ${totalMasterKg}kg`
+}
 
-  // calculo 20% 60 e 40
-  // boss
-  let sixtyPercent = weightBag * 0.6
-
-  let bossSixtyPercSc = sixtyPercent / oneBag
-  bossSixtyPercSc = bossSixtyPercSc.toFixedDown(0)
-
-  let bossSixtyPercKg = bossSixtyPercSc * oneBag - sixtyPercent
-  bossSixtyPercKg = parseInt(bossSixtyPercKg * -1)
-
-  document.getElementById(
-    'twentyPercBoss'
-  ).innerHTML = `${bossSixtyPercSc}sc e ${bossSixtyPercKg}kg`
-
-  //employee
-  let fortyPercent = weightBag * 0.4
-
-  let employeeFortyPercSc = fortyPercent / oneBag
-  employeeFortyPercSc = employeeFortyPercSc.toFixedDown(0)
-
-  let employeeFortyPercKg = employeeFortyPercSc * oneBag - fortyPercent
-  employeeFortyPercKg = parseInt(employeeFortyPercKg * -1)
-
-  document.getElementById(
-    'twentyPercEmployee'
-  ).innerHTML = `${employeeFortyPercSc}sc e ${employeeFortyPercKg}kg`
-
-  // calculo 10%  55 e 45
-  // boss
-  let fiftyFivePercent = weightBag * 0.55
-
-  let bossFiftyFivePercSc = fiftyFivePercent / oneBag
-  bossFiftyFivePercSc = bossFiftyFivePercSc.toFixedDown(0)
-
-  let bossFiftyFivePercKg = bossFiftyFivePercSc * oneBag - fiftyFivePercent
-  bossFiftyFivePercKg = parseInt(bossFiftyFivePercKg * -1)
-
-  document.getElementById(
-    'tenPercBoss'
-  ).innerHTML = `${bossFiftyFivePercSc}sc e ${bossFiftyFivePercKg}kg`
-
-  //employee
-  let fortyFivePercent = weightBag * 0.45
-
-  let employeeFortyFivePercSc = fortyFivePercent / oneBag
-  employeeFortyFivePercSc = employeeFortyFivePercSc.toFixedDown(0)
-
-  let employeeFortyFivePercKg =
-    employeeFortyFivePercSc * oneBag - fortyFivePercent
-  employeeFortyFivePercKg = parseInt(employeeFortyFivePercKg * -1)
-
-  document.getElementById(
-    'tenPercEmployee'
-  ).innerHTML = `${employeeFortyFivePercSc}sc e ${employeeFortyFivePercKg}kg`
-
-  // calculo 50%
-  let fiftyPercent = weightBag * 0.5
-
-  let FiftyFivePercSc = fiftyPercent / oneBag
-  FiftyFivePercSc = FiftyFivePercSc.toFixedDown(0)
-
-  let FiftyFivePercKg = FiftyFivePercSc * oneBag - fiftyPercent
-  FiftyFivePercKg = parseInt(FiftyFivePercKg * -1)
-
-  document.getElementById(
-    'fiftyPercentBoss'
-  ).innerHTML = `${FiftyFivePercSc}sc e ${FiftyFivePercKg}kg`
-
-  document.getElementById(
-    'fiftyPercentEmployee'
-  ).innerHTML = `${FiftyFivePercSc}sc e ${FiftyFivePercKg}kg`
-
+function calcConversion(event) {
+  event.preventDefault()
+  calculateTotalMaster('totalMaster')
+  calculate('twentyPercBoss', 0.6)
+  calculate('twentyPercEmployee', 0.4)
+  calculate('tenPercBoss', 0.55)
+  calculate('tenPercEmployee', 0.45)
+  calculate('fiftyPercentBoss', 0.5)
+  calculate('fiftyPercentEmployee', 0.5)
   open()
 }
 
-document.getElementById('conversion').addEventListener('submit', calcConversion)
+document.querySelector('form').addEventListener('submit', calcConversion)
 document.getElementById('back').addEventListener('click', close())
