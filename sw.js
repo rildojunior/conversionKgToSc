6e1294473802eba634b65d4b0dc828391ca363f0
Rildo Junior <rildojuniorcontato@gmail.com>
2026-05-30 10:29:23 -0300

    troca de versão para atualizar o PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kg-para-sc-v1';
+const CACHE_NAME = 'kg-para-sc-v2'
 const ASSETS = [
   '/',
   '/index.html',
@@ -10,27 +10,29 @@ const ASSETS = [
   '/assets/icon-512x512.png',
   '/assets/apple-touch-icon.png',
   '/assets/github.svg',
-  '/assets/instagram.svg',
-];
+  '/assets/instagram.svg'
+]
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        )
+      )
+  )
+  self.clients.claim()
+})
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
-});
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  )
+})
